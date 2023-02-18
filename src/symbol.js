ed4bed11d0697850041b6244a241c6fc1ffa0227
David Knights <david.knights@vcars.co.uk>
2023-02-18 19:30:29 +0000

    Hide visible symbol actions onBlur

--- src/symbol.js
+++ src/symbol.js
@@ -6,9 +6,11 @@ const Symbol = props => {
 
     const handleShowSymbolActions = () => setShowSymbolActions(!showSymbolActions);
 
+    const handleHideSymbolActions = () => setShowSymbolActions(false);
+
     const clickClass = props.options ? handleShowSymbolActions : null;
 
-    return <div id={props.id} className={props.class || null} onClick={clickClass}>
+    return <div id={props.id} className={props.class || null} tabIndex="0" onClick={clickClass} onBlur={handleHideSymbolActions}>
                 {showSymbolActions && (<div className="symbol-actions">
                     {props.options && (<div className="swap-symbol flex column" onClick={props.handleSwapSymbol}>
                                         <span>&rarr;</span>
