99f8ee3da6e9c5f200fc9223ad66704d3304b4d3
David Knights <david.knights@vcars.co.uk>
2023-02-25 13:23:00 +0000

    PWA versio

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './fonts/HutchinsonGuest.woff2';
 
-ReactDOMClient.createRoot(  
+ReactDOMClient.createRoot(
   document.getElementById('root')).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
 );
+
+serviceWorkerRegistration.register();
