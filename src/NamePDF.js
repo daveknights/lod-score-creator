096d81b9da213a23031a079eb3577ee8ee08c3fa
David Knights <david.knights@vcars.co.uk>
2023-02-25 13:20:14 +0000

    Set close form icon as background image

--- src/NamePDF.js
+++ src/NamePDF.js
@@ -1,8 +1,6 @@
-import remove from './images/remove-icon.svg';
-
 const NamePDF = props => (
     <form className="name-pdf-form" onSubmit={props.handleSaveScore}>
-        <img src={remove} alt="X" className="close-form" onClick={props.handleCloseNamingForm} />
+        <span className="remove-symbol close-form" onClick={props.handleCloseNamingForm}></span>
         <label htmlFor="score-name">Score name:</label>
         <input id="score-name" type="text" placeholder="lod score" value={props.scoreName} onChange={props.handleSetScoreName} />
         <button type="submit" className="name-pdf-btn btn primary-btn">Save</button>
