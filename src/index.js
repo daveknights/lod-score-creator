49e31b6bc4c0142a8137dd2f1f8f148aeaf7ae79
David Knights <david.knights@vcars.co.uk>
2023-02-27 19:30:19 +0000

    Add polyfill & woff font to support IE & older browsers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
