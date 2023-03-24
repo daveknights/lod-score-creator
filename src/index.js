107ef26e0713c1bd73b0dc4b78b6ca41d14f1b0e
David Knights <david.knights@vcars.co.uk>
2023-03-24 09:07:18 +0000

    Add new brand colours & pagination styles

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import 'react-app-polyfill/stable';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './fonts/HutchinsonGuest.woff';
 import './fonts/HutchinsonGuest.woff2';
 
 ReactDOMClient.createRoot(
